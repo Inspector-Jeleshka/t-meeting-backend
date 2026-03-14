fe07ee7d7400564fc5ce969343f2a8e2db39a028
Jeleshka <haidendark1236@gmail.com>
2026-03-14 18:57:53 +0700

    Добавил модель User

---
+++ internal/domain/user.go
@@ -0,0 +1,23 @@
+package domain
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var ErrUserNotFound = errors.New("user not found")
+
+type User struct {
+	ID           uuid.UUID
+	Email        string
+	PasswordHash string
+	Role         string
+}
+
+const (
+	AdminRole     = "admin"
+	OrganizerRole = "organizer"
+	MemberRole    = "member"
+	ViewerRole    = "viewer"
+)
