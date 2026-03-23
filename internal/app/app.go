114858fb19acd1dc46361b1bbca9e9ababad9462
TheRaindow <kozyrevraindow47@gmail.com>
2026-03-23 20:23:31 +0700

    допилил auth, подключил route

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
-
 	"t-meeting-backend/internal/adapters/postgres"
 	"t-meeting-backend/internal/config"
+	"t-meeting-backend/internal/controller"
 	"t-meeting-backend/internal/repository"
 	"t-meeting-backend/internal/route"
 	"t-meeting-backend/internal/service"
@@ -27,11 +28,27 @@ func New(ctx context.Context, cfg *config.Config) (*App, error) {
 	}
 
 	r := chi.NewRouter()
-
+	//events
 	eventRepo := repository.NewPgxEventRepository(db.Pool())
 	eventSvc := service.NewEventService(eventRepo)
-
 	route.Setup(r, eventSvc)
+
+	// users/auth
+	userRepo, err := repository.NewUserRepository(db.Pool())
+	if err != nil {
+		return nil, fmt.Errorf("new user repository: %w", err)
+	}
+
+	userSvc := service.NewUserService(userRepo)
+
+	jwtSvc := service.NewJWTService(
+		"dev-secret-change-me",
+		15*time.Minute,
+		7*24*time.Hour,
+	)
+
+	authController := controller.NewAuthController(userSvc, jwtSvc)
+	route.SetupAuthRoutes(r, authController)
 
 	return &App{
 		cfg:    cfg,
---
+++ internal/route/auth_route.go
@@ -0,0 +1,16 @@
+package route
+
+import (
+	"github.com/go-chi/chi/v5"
+
+	"t-meeting-backend/internal/controller"
+)
+
+func SetupAuthRoutes(r chi.Router, AuthController *controller.AuthController) {
+	r.Route("/auth", func(r chi.Router) {
+		r.Post("/register", AuthController.Register)
+		r.Post("/login", AuthController.Login)
+		r.Post("/refresh", AuthController.Refresh)
+		r.Get("/me", AuthController.Me)
+	})
+}
